6aa18444510b6af46e11c47fc33527615e515500
maciek04786 <maciek_sikora_1986@gmail.com>
2023-01-25 15:05:01 +0000

    Refractor code

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 import random
-from pprint import pprint
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "playlist-modify-public"
 
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-USER_ID = "USER_ID"
-
-# auth_manager = SpotifyClientCredentials()
-# spotify = spotipy.Spotify(auth_manager=auth_manager)
+USER_ID = "your ID"
 
 
-def get_playlist_id(playlist_name):
+def get_playlist_id(name):
     playlists = spotify.user_playlists(user=USER_ID)
     for playlist in playlists["items"]:
-        if playlist["name"] == playlist_name:
+        if playlist["name"] == name:
             playlist_id = playlist["id"]
             return playlist_id
         else:
             pass
 
 
-def shuffle_playlist(playlist_name):
-    playlist_id = get_playlist_id(playlist_name)
+def shuffle_playlist(name):
+    playlist_id = get_playlist_id(name)
     tracks = spotify.playlist_items(playlist_id=playlist_id)
     track_list = [track["track"]["id"] for track in tracks["items"]]
     random.shuffle(track_list)
 
-    new_name = f"{playlist_name}"
+    new_name = f"{name}"
     new_playlist = spotify.user_playlist_create(user=USER_ID, name=new_name, public=True)
     new_playlist_id = new_playlist["id"]
 
     spotify.playlist_add_items(playlist_id=new_playlist_id, items=track_list)
 
 
-shuffle_playlist("Playlist name")
+playlist_name = input("Type in name of the playlist: ")
+
+shuffle_playlist(playlist_name)
